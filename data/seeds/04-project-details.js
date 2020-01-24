7d44a75fe4e9a8fc63423e52480f48738fbd7c6c
brittanymae01 <brittanymae01>
2020-01-24 08:49:44 -0800

    created schema in migrations and populated seeds

---
+++ data/seeds/04-project-details.js
@@ -0,0 +1,10 @@
+exports.seed = function(knex) {
+  return knex("project_details").insert([
+    { project_id: 1, resources_id: 3 },
+    { project_id: 1, resources_id: 4 },
+    { project_id: 2, resources_id: 1 },
+    { project_id: 2, resources_id: 2 },
+    { project_id: 3, resources_id: 5 },
+    { project_id: 3, resources_id: 6 }
+  ]);
+};
